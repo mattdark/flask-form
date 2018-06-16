9926d9a1cd60361f60ce46e107e08b005ac8b20d
Mario Garcia <iscmariog@gmail.com>
2018-06-16 14:09:42 -0500

    Error fixed

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template, request, flash
+from flask import Flask, render_template, request, flash
 from forms import *
 
 app = Flask(__name__)
@@ -8,13 +7,12 @@ app.secret_key = 'development key'
 @app.route('/', methods = ['GET', 'POST'])
 def hello():
     form = Registro()
+
     if request.method == 'POST':
-        if form.validate() == 'False':
-            flash('All fields are required')
-            render_template('index.html', form = form)
+        if form.validate() == False:
+            return render_template('index.html', form = form)
         else:
             name, last_name, email = form.name.data, form.last_name.data, form.email.data
-            submission_successful = True
             return render_template('registro.html', name = name, last_name = last_name, email = email)
     elif request.method == 'GET':
         return render_template('index.html', form = form)
